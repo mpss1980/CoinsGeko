f5d197803bd5cbcdbe3f18b100f35c80df9f0bc2
Marcos Sousa <marcosp.sousa@gmail.com>
2025-01-01 16:16:28 -0300

    Initial Commit

---
+++ CoinsGeko/CoinsGekoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsGekoApp.swift
+//  CoinsGeko
+//
+//  Created by Field on 01/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CoinsGekoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
